591e1dbd57cb7919ec244da1d92cc60db53a1ec9
Toby Brandon <84815613+sumkindafreak@users.noreply.github.com>
2025-04-19 14:58:32 +0100

    Delete HauntSyncForum_Configured directory

--- HauntSyncForum_Configured/forum.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCjdh1wafL8ukJgucbya9X8kzQQtP2WPWw",
-  authDomain: "hauntsync-forum-4b992.firebaseapp.com",
-  projectId: "hauntsync-forum-4b992",
-  storageBucket: "hauntsync-forum-4b992.appspot.com",
-  messagingSenderId: "165297838270",
-  appId: "1:165297838270:web:1e4a3278e2d7de23b92490"
-};
-
